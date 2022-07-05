6630334b63387d87d55bad31b81545fd39c2ddd2
ThangarajManthiriyappan <tmanthiriyappan@fleetstudio.com>
2022-07-05 17:09:13 +0530

    hello world

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,redirect,url_for
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,6 +7,24 @@ def welcome():
 @app.route('/admin')
 def welcome1():
     return 'Hello admin!!'
+
+@app.route('/pass/<int:score>')
+def success(score):
+    return 'The person has passed and got '+str(score) +'marks'
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return 'The person had failed and got '+str(score) +'marks'
+
+@app.route('/results/<int:score>')
+def results(score):
+    result=""
+    if score <50:
+        result='fail'
+    else:
+        result='pass'
+
+    return result
 
 if __name__ == '__main__':
     app.run(debug =True)
