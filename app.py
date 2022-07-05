54ba57d1bcf185c17fd6daa320331e4082b78351
ThangarajManthiriyappan <tmanthiriyappan@fleetstudio.com>
2022-07-05 16:50:05 +0530

    hello world

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return 'Hello world!!'
+@app.route('/admin')
+def welcome1():
+    return 'Hello admin!!'
+
+if __name__ == '__main__':
+    app.run(debug =True)
